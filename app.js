993c7770dfed5cfb295193ccec893f415a96587f
PCThomas7 <support@professorpcthomas.com>
2025-03-24 10:48:58 +0530

    Fix Question Upload Json limit to up 50mb

--- app.js
+++ app.js
@@ -73,6 +73,13 @@ import questionRoutes from './routes/questionRoutes.js';
 import tagRoutes from './routes/TagRoutes.js';
 import QuizRoutes from './routes/QuizRoutes.js';
 
+
+// Increase payload size limit
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+
+
 // Route middleware
 app.use('/api/auth', authRoutes);
 app.use('/api/batches', batchRoutes);
